a2d33388f4e1864c844da333ab2753e21d39d4e1
Jakraes <70046168+Jakraes@users.noreply.github.com>
2023-04-04 05:07:04 +0100

    Bué comentários

--- Main.scala
+++ Main.scala
@@ -13,10 +13,15 @@ object Main extends App {
     case x::xs => getIndexInList(x, value).map(y => (y, accY)):::getIndexInMatrix(xs, value, accY + 1)
   }
 
+  def filterToBounds[T](l: List[List[T]], i: List[(Int, Int)]): List[(Int, Int)] = {
+    i.filter{case (x, y) => x == 0 || x == l.size - 1 || y == 0 || y == l.size - 1}
+  }
+
+
   val l = List(
     List(3, 2, 1),
     List(5, 3, 3),
     List(3, 1, 2)
   )
-  println(getIndexInMatrix(l, 3)
+  println(filterToBounds(l, getIndexInMatrix(l, 3)))
 }
