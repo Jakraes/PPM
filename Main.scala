ce8da450bb5678ef3edba0dfed295fdd5cd0a08f
Jakraes <70046168+Jakraes@users.noreply.github.com>
2023-04-05 05:53:40 +0100

    Comentários 2

--- Main.scala
+++ Main.scala
@@ -2,7 +2,7 @@ object Main {
   def main(args: Array[String]) = {
     // Mudei a criação do MyRandom e do MyInput para a main para ser mais puro, o que acham? - João C
     val io = new MyIO
-    val rand = new MyRandom(0) // Aqui temos que
+    val rand = new MyRandom(0)
     val game = Game(rand, io)
     game.start
   }
