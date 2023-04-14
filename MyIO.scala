f24aeb9f36f2dcc1c639acaa6a586609cb6f2828
pamen <p.a.mendonc@gmail.com>
2023-04-14 20:06:53 +0100

    fixei a estrutura

--- MyIO.scala
+++
@@ -1,9 +0,0 @@
-case class MyIO() { //TODO object? Não pode por causa das coisas da pureza + erros no resto do código
-  def getLine: String = scala.io.StdIn.readLine
-
-  def getInt: Int = scala.io.StdIn.readInt
-
-  def print(s: Any) = scala.Predef.print(s)
-
-  def println(s: Any) = scala.Predef.println(s)
-}
