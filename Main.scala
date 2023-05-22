bb6d7fef49177a9774d818f81089bfa201fb69fc
Jakraes <70046168+Jakraes@users.noreply.github.com>
2023-05-22 03:52:21 +0100

    Pasta src bazou 👍

---
+++ Main.scala
@@ -0,0 +1,8 @@
+object Main extends App{
+    // Mudei a criação do MyRandom e do MyInput para a main para ser mais puro, o que acham? - João C
+<<<<<<<< HEAD:test/scala/Main.scala
+   Game(new MyRandom(0), 5).start
+========
+   Game(new MyRandom(0)).start
+>>>>>>>> 2f3906e4b3a84af773000eb0528aa02b7d698ea4:src/main/scala/Main.scala
+}
